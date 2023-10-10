c437d19fccfcb92d0787a0d5a9b8d7633a22b458
RZR-UA <youonly@ukr.net>
2023-10-10 23:33:40 +0200

    Lab: test version of aStar()

---
+++ map/src/lab/aStar.mjs
@@ -0,0 +1,97 @@
+import { getDistanceEuclidean } from './utils.mjs';
+
+class Node {
+    constructor(ll) {
+        this.ll = ll;
+        this.parent = null; // parent (Node class)
+        this.fromStart = Infinity; // cummulative weight from the start (g)
+        this.toEnd = 0; // weight from the start + heuristic-to-the-end (to sort queue) (f)
+    }
+}
+
+export function aStar({ graph, startNodeLL, finishNodeLL }) {
+    const openNodes = []; // TODO https://github.com/mourner/tinyqueue
+    const closedNodes = new Set();
+
+    const debugViewed = new Set();
+    const debugQueued = new Set();
+
+    const finish = new Node(finishNodeLL);
+    const start = new Node(startNodeLL);
+    start.fromStart = 0; // toEnd ?
+    openNodes.push(start);
+
+    while (openNodes.length > 0) {
+        openNodes.sort((a, b) => a.toEnd - b.toEnd); // XXX: use priority queue (binary heap) instead of sort()
+
+        const current = openNodes.shift();
+        closedNodes.add(current.ll);
+
+        // finally
+        if (current.ll === finishNodeLL) {
+            const path = [];
+            let temp = current;
+            while (temp) {
+                path.unshift(temp);
+                temp = temp.parent;
+            }
+            const points = pathSegmentsGeometry(path);
+            // console.log(debugViewed.size); // XXX
+            // console.log(debugQueued.size); // XXX
+            // console.log(path);
+            return { points };
+        }
+
+        for (const edge of graph[current.ll] || []) {
+            const edgeLL = edge.node;
+            debugViewed.add(edgeLL);
+
+            // already closed - skip
+            if (closedNodes.has(edgeLL)) {
+                continue;
+            }
+
+            // search and use the found one or create a new neighbor
+            const found = openNodes.find((node) => node.ll === edgeLL); // ref
+            const neighbor = found ?? new Node(edgeLL);
+
+            // check and update shorter way
+            const preliminaryFromStart = current.fromStart + edge.weight;
+            if (preliminaryFromStart < neighbor.fromStart) {
+                neighbor.fromStart = preliminaryFromStart;
+                neighbor.toEnd = neighbor.fromStart + heuristic(neighbor, finish);
+                // neighbor.parent = current; // XXX
+            }
+
+            // add to queue if not found
+            if (!found) {
+                neighbor.parent = current;
+                neighbor.segment = edge.segment;
+                debugQueued.add(neighbor.ll);
+                openNodes.push(neighbor);
+            }
+        }
+    }
+
+    return null; // failed
+}
+
+function heuristic(nodeA, nodeB) {
+    // if (nodeA) {
+    //     return 0;
+    // }
+    const [latA, lngA] = nodeA.ll.split(',');
+    const [latB, lngB] = nodeB.ll.split(',');
+    return getDistanceEuclidean(latA, lngA, latB, lngB);
+}
+
+function pathSegmentsGeometry(path) {
+    let points = [];
+    for (const node of path) {
+        if (node.segment) {
+            points = points.concat(node.segment);
+        }
+    }
+    // console.log(points);
+    return points;
+}
